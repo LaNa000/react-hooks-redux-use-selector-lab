91ca313fd32423a63932edf75076cd9e251e6530
lana <lana.km98@gmail.com>
2021-08-30 13:06:49 +0300

    soultion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
 import usersReducer from "./features/users/usersSlice";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-// add imports and code
+const store = createStore(usersReducer);
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
-  // add imports and code
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById("root")
 );
